747c7851507517231abf83a11f0a4301bf0ba5ca
David Nuon <david@davidnuon.com>
2018-05-03 23:12:14 -0700

    Added code

---
+++ android/app/src/main/kotlin/com/davidnuon/otamatonefanapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.davidnuon.otamatonefanapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
